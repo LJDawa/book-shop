fac8db827f183c6740cb8d700cec15ea75d94418
LovejoyDawa <le.eskinder@gmail.com>
2018-08-18 17:35:08 +0200

    Navbar and footer added with css style

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers/index";
-import { addToCart } from "./actions/cartActions";
-import { postBooks, deleteBooks, updateBooks } from "./actions/booksActions";
+
 import logger from "redux-logger";
 
 //STEP 1 create the store
@@ -13,10 +12,16 @@ const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 import BooksList from "./components/pages/booksList";
+import Menu from "./components/menu";
+import Footer from "./components/footer";
 
 render(
   <Provider store={store}>
-    <BooksList />
+    <div>
+      <Menu />
+      <BooksList />
+      <Footer />
+    </div>
   </Provider>,
   document.getElementById("app")
 );
